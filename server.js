ca74e9187ba3e2f35c979c0da2678e370471ffcf
anuj2019104014 <ganuj754@gmail.com>
2023-07-09 01:33:44 +0530

    add status-code and redirection p-5

--- server.js
+++ server.js
@@ -10,10 +10,18 @@ const server=http.createServer((req,res)=>{
     let path='./views'
     switch(req.url){
         case '/':path+='/index.html'
+        res.statusCode=200
         break;
         case '/about':path+='/about.html'
+        res.statusCode=200
+       break;
+       case '/about-me':path+='/about.html'
+        res.statusCode=301
+        res.setHeader('Location','/about');
+        res.end();
        break;
        default:path+='/404.html';
+       res.statusCode=404
        break;
     }
     fs.readFile(path,(err,fillData)=>{
